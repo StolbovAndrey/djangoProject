3fbf756aff82ab5241d03eb3dc4e8b20e8b0a880
stolbovAE <andy_y_s@mail.ru>
2022-02-28 23:50:22 +0300

    Добавлены стили сообщений пользователю в Products

--- baskets/views.py
+++ baskets/views.py
@@ -10,11 +10,16 @@ def basket_add(request, product_id):
     baskets = Basket.objects.filter(user=request.user, product=product)
     if not baskets:
         Basket.objects.create(user=request.user, product=product, quantity=1)
+        messages.success(request, f'Товар {product.name} добавлен в корзину')
         return HttpResponseRedirect(request.META['HTTP_REFERER'])
     else:
         basket = baskets.first()
-        basket.quantity += 1
-        basket.save()
+        if basket.quantity >= product.quantity:
+            messages.warning(request, 'на складе отсутствует данное количество товара')
+        else:
+            basket.quantity += 1
+            basket.save()
+            messages.success(request, f'Товар {product.name} добавлен в корзину')
         return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
 
