2530bd0914b81cdc5743f9fddad3018ccc85e15e
stolbovAE <andy_y_s@mail.ru>
2022-04-04 18:46:00 +0300

    last

--- admins/views.py
+++ admins/views.py
@@ -1,73 +1,79 @@
-from django.contrib import messages
-from django.shortcuts import render, HttpResponseRedirect
-from django.urls import reverse
 from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
+from django.utils.decorators import method_decorator
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
 
 from admins.forms import UserAdminRegForm, UserAdminProfileForm
 from users.models import User
 
 
-@user_passes_test(lambda u: u.is_staff or u.is_superuser, login_url='/')
-def index(request):
-    context = {
-        'title': 'Админ-панель',
-    }
-    return render(request, 'admins/index.html', context)
+class TitleMixin:
+    title = None
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(TitleMixin, self).get_context_data(object_list=None, **kwargs)
+        context['title'] = self.title
+        return context
 
 
-@user_passes_test(lambda u: u.is_staff or u.is_superuser, login_url='/')
-def admin_users(request):
-    users = User.objects.all()
-    context = {
-        'title': 'Админ-панель',
-        'users': users
-    }
-    return render(request, 'admins/admin-users-read.html', context)
+class IndexAdminListView(TitleMixin, ListView):
+    model = User
+    template_name = 'admins/index.html'
+    title = "Главная страница"
+
+    @method_decorator(user_passes_test(lambda u: u.is_staff or u.is_superuser, login_url='/'))
+    def dispatch(self, request, *args, **kwargs):
+        return super(IndexAdminListView, self).dispatch(request, *args, **kwargs)
 
 
-@user_passes_test(lambda u: u.is_staff or u.is_superuser, login_url='/')
-def admin_users_create(request):
-    if request.method == 'POST':
-        form = UserAdminRegForm(data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Пользователь зарегистрирован')
-            return HttpResponseRedirect(reverse('admin_staff:admin_users'))
-    else:
-        form = UserAdminRegForm()
+class UserAdminListView(TitleMixin, SuccessMessageMixin, ListView):
+    model = User
+    template_name = 'admins/admin-users-read.html'
+    title = 'Админка - пользователи'
 
-    context = {
-        'title': 'Админ-панель',
-        'form': form
-    }
-    return render(request, 'admins/admin-users-create.html', context)
+    @method_decorator(user_passes_test(lambda u: u.is_staff or u.is_superuser, login_url='/'))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserAdminListView, self).dispatch(request, *args, **kwargs)
 
 
-@user_passes_test(lambda u: u.is_staff or u.is_superuser, login_url='/')
-def admin_users_update(request, pk):
-    selected_user = User.objects.get(id=pk)
+class UserAdminCreateView(TitleMixin, SuccessMessageMixin, CreateView):
+    model = User
+    form_class = UserAdminRegForm
+    template_name = 'admins/admin-users-create.html'
+    success_url = reverse_lazy('admin_staff:admin_users')
+    success_message = 'Пользователь создан'
+    title = "Создание пользователя"
 
-    if request.method == 'POST':
-        form = UserAdminProfileForm(instance=selected_user, data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Данные успешно обновлены')
-            return HttpResponseRedirect(reverse('admin_staff:admin_users'))
-    else:
-        form = UserAdminProfileForm(instance=selected_user)
-
-    context = {
-        'title': 'Админ-панель',
-        'form': form,
-        'selected_user': selected_user
-    }
-
-    return render(request, 'admins/admin-users-update-delete.html', context)
+    @method_decorator(user_passes_test(lambda u: u.is_staff or u.is_superuser, login_url='/'))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserAdminCreateView, self).dispatch(request, *args, **kwargs)
 
 
-@user_passes_test(lambda u: u.is_staff or u.is_superuser, login_url='/')
-def admin_users_delete(request, pk):
-    selected_user = User.objects.get(id=pk)
-    selected_user.safe_delete()
-    messages.warning(request, f'Пользователь {selected_user.username} удален')
-    return HttpResponseRedirect(reverse('admin_staff:admin_users'))
+class UserAdminUpdateView(TitleMixin, UpdateView):
+    model = User
+    form_class = UserAdminProfileForm
+    template_name = 'admins/admin-users-update-delete.html'
+    success_url = reverse_lazy('admin_staff:admin_users')
+    title = "Обновление информации о пользователе"
+
+    @method_decorator(user_passes_test(lambda u: u.is_staff or u.is_superuser, login_url='/'))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserAdminUpdateView, self).dispatch(request, *args, **kwargs)
+
+
+class UserAdminDeleteView(DeleteView):
+    model = User
+    template_name = 'admins/admin-users-update-delete.html'
+    success_url = reverse_lazy('admin_staff:admin_users')
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.safe_delete()
+        return HttpResponseRedirect(self.success_url)
+
+    @method_decorator(user_passes_test(lambda u: u.is_staff or u.is_superuser, login_url='/'))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserAdminDeleteView, self).dispatch(request, *args, **kwargs)
--- products/views.py
+++ products/views.py
@@ -1,23 +1,35 @@
 from django.shortcuts import render
 
 from products.models import ProductCategory, Products
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def index(request):
     context = {'title': 'GeekShop'}
     return render(request, 'products/index.html', context)
 
 
-def products(request):
+def products(request, category_id=None, page=1):
 
     categories = ProductCategory.objects.all()
-    all_products = Products.objects.all()
     baskets = None
     context = {
         'title': 'Каталог товаров',
-        'products': all_products,
         'categories': categories,
         'baskets': baskets
     }
+    if category_id:
+        products = Products.objects.filter(category_id=category_id)
+    else:
+        products = Products.objects.all()
+
+    pag = Paginator(products, 3)
+    try:
+        products_pag = pag.page(page)
+    except PageNotAnInteger:
+        products_pag = pag.page(1)
+    except EmptyPage:
+        products_pag = pag.page(pag.num_pages)
+
+    context['products'] = products_pag
 
     return render(request, 'products/products.html', context)
